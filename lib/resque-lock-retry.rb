4edf87a39b70b5c140b3260214e9ebc20fa9d7ee
Ryan Carver <ryan@fivesevensix.com>
2010-03-18 15:35:24 -0700

    don't need PerformInternal anymore

--- lib/resque-lock-retry.rb
+++ lib/resque-lock-retry.rb
@@ -1,5 +1,5 @@
 require 'resque'
-require 'resque/jobs/perform_internal'
+require 'resque/job_callbacks'
 require 'resque/jobs/locked'
 require 'resque/jobs/retried'
 require 'resque/jobs/retry_on_lock'
